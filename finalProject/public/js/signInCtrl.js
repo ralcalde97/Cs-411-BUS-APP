3266e0782040870e3fffc84870e83ca735e0584f
ralcalde97 <34291131+ralcalde97@users.noreply.github.com>
2018-04-30 13:39:20 -0400

    Add files via upload

---
+++ finalProject/public/js/signInCtrl.js
@@ -0,0 +1,34 @@
+var signInCtrl = angular.module('signInCtrl', []);
+signInCtrl.controller('signInCtrl', function($scope, $http){
+
+
+    // Functions
+    // ----------------------------------------------------------------------------
+
+    // Creates a new user based on the form fields
+    $scope.isUser = function() {
+
+        // Grabs all of the text box fields
+        var userData = {
+            username: $scope.formData.username,
+            password: $scope.formData.password,
+        };
+
+        // Saves the user data to the db
+        $http.get('/users', userData)
+            .success(function (data) {
+
+                // Once complete, clear the form (except location)
+                $scope.formData.username = "";
+                $scope.formData.password = "";
+
+                // Refresh the map with new data
+                gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    
+});
