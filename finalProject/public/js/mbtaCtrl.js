157a2ea382713d3228e5c996fd13712f2d7d5e25
epazoff <epazoff@bu.edu>
2018-05-01 16:24:57 -0400

    added inbound

--- finalProject/public/js/mbtaCtrl.js
+++ finalProject/public/js/mbtaCtrl.js
@@ -9,12 +9,19 @@ mbtaCtrl.controller('mbtaCtrl', function($scope, $log, $http) {
     $scope.testVariable = 0
     $scope.arrivalTime = ""
     $scope.stops=[{stopname: "Blandford St. Outbound", stopnum: "70149"},
+    {stopname: "Blandford St. Inbound", stopnum: "70148"},
     {stopname: "BU East Outbound", stopnum: "70147"},
+    {stopname: "BU East Inbound", stopnum: "70146"},
     {stopname: "BU Central Outbound", stopnum: "70145"},
+    {stopname: "BU Central Inbound", stopnum: "70144"},
     {stopname: "BU West Outbound", stopnum: "70143"},
+    {stopname: "BU West Inbound", stopnum: "70142"},
     {stopname: "St. Paul St. Outbound", stopnum: "70141"},
+    {stopname: "St. Paul St. Inbound", stopnum: "70140"},
     {stopname: "Pleasant St. Outbound", stopnum: "70139"},
-    {stopname: "Babcock St. Outbound", stopnum: "70137"}]
+    {stopname: "Pleasant St. Inbound", stopnum: "70138"},
+    {stopname: "Babcock St. Outbound", stopnum: "70137"},
+    {stopname: "Babcock St. Inbound", stopnum: "70136"}]
     $scope.stopData = function(val)  {
         var num = $scope.SelectedStop.stopnum;
         queryBody = {
