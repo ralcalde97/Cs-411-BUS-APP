68dd403ffa8c8678ff7501ecca911cc83c005ade
epazoff <epazoff@bu.edu>
2018-05-03 13:00:54 -0400

    added comments

--- finalProject/public/js/mbtaCtrl.js
+++ finalProject/public/js/mbtaCtrl.js
@@ -1,13 +1,13 @@
 var mbtaCtrl = angular.module('mbtaCtrl', []);
 mbtaCtrl.controller('mbtaCtrl', function($scope, $log, $http) {
 
-    // Initializes Variables
-    // ----------------------------------------------------------------------------
     $scope.formData = {};
 
+    //Initializing variables that are used in the function
     $scope.num = 0
     $scope.testVariable = 0
     $scope.arrivalTime = ""
+    //List of all stops along BU's campus (inbound and outbound), along with ID # to be called by MBTA API
     $scope.stops=[{stopname: "Blandford St. Outbound", stopnum: "70149"},
     {stopname: "Blandford St. Inbound", stopnum: "70148"},
     {stopname: "BU East Outbound", stopnum: "70147"},
@@ -22,24 +22,27 @@ mbtaCtrl.controller('mbtaCtrl', function($scope, $log, $http) {
     {stopname: "Pleasant St. Inbound", stopnum: "70138"},
     {stopname: "Babcock St. Outbound", stopnum: "70137"},
     {stopname: "Babcock St. Inbound", stopnum: "70136"}]
+    
+    //one function that can be used for any stop
     $scope.stopData = function(val)  {
+        //stop ID
         var num = $scope.SelectedStop.stopnum;
         queryBody = {
             stopID: num
         };
 
+        //calls MBTA API through routes.js
         $http.post('/mbtaAPI', queryBody)
 
             // Store the filtered results in queryResults
             .success(function(queryResults){
+                //proof that it called the API
                 console.log(queryResults);
                 $scope.arrivalTime = queryResults;
             })
+        //triggers the "____ train will arrive at ____ time" phrase in html file
         $scope.testVariable = 5
 
-    }
-    $scope.clear = function() {
-        $scope.arrivalTime = "";
     }
 
 });
